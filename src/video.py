c5333041ad6d5efbec439dc555c9b3b1875ccc49
Pvl <ivanov.pavel.1307@gmail.com>
2023-06-14 00:53:37 +0200

    HM4

---
+++ src/video.py
@@ -0,0 +1,28 @@
+from googleapiclient.discovery import build
+import os
+
+
+class Video:
+    def __init__(self, video_id):
+        api_key: str = os.getenv('YT_API_KEY')
+
+        youtube = build('youtube', 'v3', developerKey=api_key)
+
+        self.__video_id = video_id
+
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id).execute()
+
+        self.video_title: str = video_response['items'][0]['snippet']['title']
+        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
